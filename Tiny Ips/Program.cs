2b2ee575f819bfc03ddca99cdea169e06945f9a1
Michael <admin@codeisle.com>
2013-11-25 22:02:47 -0500

    Initial commit.

---
+++ Tiny Ips/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using CodeIsle.LibIpsNet;
+namespace TinyIps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            try
+            {
+
+                string command = string.Empty;
+
+                string patch = string.Empty;
+                string target = string.Empty;
+
+                string original = string.Empty;
+                string modified = string.Empty;
+
+                string output = string.Empty;
+
+                if (args.Length > 4 || args.Length == 0)
+                {
+                    PrintUsage();
+                    return;
+                }
+
+                command = args[0];
+             
+                switch(command)
+                {
+                    case "apply":
+                    case "a":
+                        
+
+                        if (args.Length > 2)
+                        {
+                            patch = args[1];
+                            target = args[2];
+                        }
+                        else
+                        {
+                            PrintUsage();
+                            return;
+                        }
+
+                        if(args.Length > 3) 
+                        {
+                            output = args[3];
+                        }
+
+                        if (output == string.Empty)
+                        {
+                            output = target;
+                        }
+
+                        Patcher patcher = new Patcher();
+
+                        patcher.Patch(patch, target, output);
+
+                        return;
+                    case "create":
+                    case "c":
+
+                        if (args.Length == 4)
+                        {
+                            original = args[1];
+                            modified = args[2];
+                            output = args[3];
+                        }
+                        else
+                        {
+                            PrintUsage();
+                            return;
+                        }
+                        Creator creator = new Creator();
+                        creator.Create(original, modified, output);
+                        return;
+                }
+                PrintUsage();
+                return;
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        static void PrintUsage()
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+            Console.WriteLine("{0} {1}", versionInfo.ProductName, versionInfo.ProductVersion.ToString());
+            Console.WriteLine("{0}", versionInfo.LegalCopyright);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Usage:");
+            Console.WriteLine("tinyips apply|a <patch> <target> [<output>]");
+            Console.WriteLine("tinyips create|c <original> <modified> <output>");
+            // Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().get)
+        }
+    }
+}
---
+++ Tiny Ips/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tinyIPS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tinyIPS")]
+[assembly: AssemblyCopyright("Copyright {c}  2013 CodeIsle.com. Uses the library LibIPS.NET, licensed under the GNU Lesser General Public License, version 3.0 or higher. Copyright (c) 2013 CodeIsle.com, Copyright (c) 2012 Alcaro.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a1c1f69-a7b0-423f-a100-629826fea9dc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
